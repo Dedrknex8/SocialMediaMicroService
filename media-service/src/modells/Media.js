4686078f85f3d1da5018c7c69bc75ab5696fe270
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-02-19 07:16:53 +0530

    added cloudinary setup

---
+++ media-service/src/modells/Media.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const mediaSchema = new mongoose.Schema({
+    publicId :{
+        type:String,
+        required: true,
+    },
+    originalName : {
+        type : String,
+        requied : true,
+    },
+    MimeType : {
+        type : String,
+        requied : true,
+    },
+    url : {
+        type : String,
+        requied : true,
+    },
+    userId : {
+        type  : mongoose.Types.ObjectId,
+        ref : 'user',
+        required:true,
+    }
+},{timestamps:true});
+
+const Media = mongoose.model('Model',mediaSchema);
---
+++ media-service/src/utils/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+const logger = require('./logger');
+
+
+cloudinary.config({
+    cloud_name : ,
+    api_key : ,
+    api_secret : ,
+})
